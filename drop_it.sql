6aef98b18231d8320bd85ae648b8c07abf4eb31d
PrideTheDaemon <hellangel@live.fr>
2020-04-10 17:48:32 +0200

    Commented drop_it

--- drop_it.sql
+++ drop_it.sql
@@ -1,19 +1,24 @@
+-- drops everything in the database
+-- triggers
 DROP TRIGGER trg_adt_report;
 DROP TRIGGER trg_adt_keyword;
 DROP TRIGGER trg_report_deadline;
 DROP TRIGGER trg_report_validation;
 DROP TRIGGER trg_teacher_hired_date;
 
+-- procedures
 DROP PROCEDURE prc_delete_intermediary_reports;
 DROP PROCEDURE prc_report_download;
 DROP PROCEDURE prc_report_consult;
 DROP PROCEDURE prc_report_copy;
 DROP PROCEDURE prc_report_print;
 
+-- functions
 DROP FUNCTION fun_is_allowed;
 DROP FUNCTION fun_reports_by_keyword;
 DROP FUNCTION fun_most_wanted_reports;
 
+-- data tables, relations tables and audit tables
 DROP TABLE adt_keyword;
 DROP TABLE adt_report;
 DROP TABLE rel_has;
